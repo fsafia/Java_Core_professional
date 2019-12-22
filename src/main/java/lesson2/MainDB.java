fc4396955536fa83fbf0c46fc6f25209afaf9404
fsafia <fsafia@yandex.ru>
2019-12-22 16:48:34 +0300

    методы создания удаления изменения БД

---
+++ src/main/java/lesson2/MainDB.java
@@ -0,0 +1,105 @@
+package lesson2;
+
+import java.sql.*;
+
+public class MainDB {
+    private static Connection connection;
+    private static Statement stmt;
+
+    public static void main(String[] args) {
+        try {
+            connect();
+
+            createTable("parents", "id", "name");
+//            insertToParents(1, "Alex");
+//            insertToParents(2, "Max");
+//            insertToParents(3, "Max");
+//            updateToParents(4, "Anna", 2);
+//            deleteFromParents(4);
+//            selectFromParents(3 );
+//            deleteTable("parents");
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            disconnect();
+        }
+
+    }
+
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+        stmt = connection.createStatement();
+    }
+
+    public static void disconnect(){
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createTable(String tableName, String idColumnName, String textColumnName){
+
+        String sql = String.format("CREATE TABLE `%s` ( `%s` INTEGER, `%s` TEXT );", tableName, idColumnName, textColumnName);
+        try {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteTable(String tableName){
+        String sql = String.format("DROP TABLE IF EXISTS '%s'", tableName);
+        try {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void insertToParents(int id, String name ){
+        try {
+            String sql = String.format("INSERT INTO parents (id, name) VALUES (%s, '%s' )", id, name);
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void updateToParents(int idNew, String newName, int id ){
+        try {
+            String sql = String.format("UPDATE parents SET id = %s, name = '%s' WHERE id = %s", idNew, newName, id);
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteFromParents(int id ){
+        try {
+            String sql = String.format("DELETE FROM parents  WHERE id = %s", id);
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectFromParents(int id ){
+        try {
+            String sql = String.format("SELECT id, name FROM parents  WHERE id = %s", id);
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()){
+                System.out.println("id = " + rs.getInt(1) + "  name = " + rs.getString("name"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
